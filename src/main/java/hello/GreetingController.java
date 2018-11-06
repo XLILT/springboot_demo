7c0c6fc170d848cbbd70e12ba19b6011fd3664e6
maxiaolong <maxiaolong@sunlands.com>
2018-11-06 11:53:55 +0800

    init

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,33 @@
+/*************************************************************************
+* COPYRIGHT NOTICE
+*  Copyright (c) 2018, Wuhan Live Youxuan Online Education Co., Ltd.
+*  All rights reserved.
+*
+*  @version : 1.0
+*  @author : mxl
+*  @E-mail : xiaolongicx@gmail.com
+*  @date : 2018-11-06 09:59
+*
+*  Revision Notes :
+*/
+
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
+
