5ae13a61fa0013857efe07f6b679bdb4ff2aedd3
maxiaolong <maxiaolong@sunlands.com>
2018-11-06 13:35:22 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+################################################
+# File Name: init.sh
+# Author: mxl
+# Mail: xiaolongicx@gmail.com
+# Created Time: 2018-11-06 13:33
+################################################
+
+mvn spring-boot:run
